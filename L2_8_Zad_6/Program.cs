8e20c91ded23dfac53afc7c1a6607ab9754454f4
Damian Z <damian9203@gmail.com>
2021-12-19 17:36:12 +0100

    Add project files.

---
+++ L2_8_Zad_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L2_8_Zad_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program dla liczb od 0 do 20 obliczy sumę wg wzoru: 1 + 1/2 + 1/3 + 1/4 itd.");
+            
+            decimal sumOfThePattern = 0;
+            
+            for (decimal i = 1; i <= 20; i++)
+            {
+                decimal componentOfThePattern = 1 / i;
+                sumOfThePattern = sumOfThePattern + componentOfThePattern;
+            }
+            Console.WriteLine($"Suma liczb ze wzoru wynosi: {sumOfThePattern}");
+        }
+    }
+}
